e68db113d103c24b7faa69f73dda831b201bfe85
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-06-08 23:32:17 +0900

    fix: train-ts h-task

--- train.ts
+++ train.ts
@@ -2,7 +2,7 @@
 
  //shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
-function getPositive(a) {
+ function getPositive(a: number[]): string {
     let result = '';
     for (let i = 0; i < a.length; i++) {
         if (a[i] > 0) {
@@ -12,4 +12,4 @@ function getPositive(a) {
     return result;
 }
 
-console.log(getPositive([7, -8, 4])); 
+console.log(getPositive([7, -8, 4])); // Output: "74"
