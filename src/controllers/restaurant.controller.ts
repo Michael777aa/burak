44e9b23797e9c373fbfd2a2c47754d2bec3d738f
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-06-16 17:13:44 +0900

    feat: Bcrypt lessons

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -4,7 +4,7 @@ import {T} from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
-
+import {LoginInput} from "../libs/types/member";
 
 const restaurantController: T = {}; 
 restaurantController.goHome = (req: Request, res: Response) => {
@@ -41,12 +41,17 @@ restaurantController.getSignup = (req: Request, res: Response) => {
  
 
 
-restaurantController.processLogin = (req: Request, res: Response) => {
+restaurantController.processLogin = async (req: Request, res: Response) => {
     try {
         console.log("processLogin");
-        res.send("DONE");
+        const input: LoginInput = req.body; 
+
+        const memberService = new MemberService();
+        const result = await memberService.processLogin(input);
+        res.send(result);
     }catch(err) {
         console.log("Error, processLogin:", err);
+        res.send(err);
         
     }
 };
@@ -59,10 +64,13 @@ restaurantController.processSignup = async (req: Request, res: Response) => {
 
         const newMember: MemberInput = req.body;
         newMember.memberType = MemberType.RESTAURANT;
-
+       
 
         const memberService = new MemberService();
         const result = await memberService.processSignup(newMember);
+       
+        
+        
         
         res.send(result);
     }catch(err) {
