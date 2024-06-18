963d973c5ed7ba316b90a4b6114641bd402d5d82
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-06-18 18:16:39 +0900

    feat: develop admin frontend and integrated pages

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -13,7 +13,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
     try {
         console.log('goHome');
         
-        res.send("Home Page")
+        res.render("Home")
     }catch(err) {
         console.log("Error, goHome:", err);
         
@@ -23,7 +23,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
     try {
         console.log("getSignup");
-        res.send("Signup Page")
+        res.render("signup")
     }catch(err) {
         console.log("Error, getSignup:", err);
         
@@ -34,7 +34,7 @@ restaurantController.getLogin = (req: Request, res: Response) => {
     try {
         console.log("getLogin");
         
-        res.send("Login Page")
+        res.render("login")
     }catch(err) {
         console.log("Error, getLogin:", err);
         
--- train.ts
+++ train.ts
@@ -1,33 +1,4 @@
+// J-TASK: 
 
-// I-TASK:
-
-// Shunday function yozing, u parametridagi array ichida eng kop takrorlangan raqamni topib qaytarsin.
-// MASALAN: majorityElement([1,2,3,4,5,4,3,4]) return 4
-
-
-function majorityElement(arr: number[]): number {
-    const countMap: { [key: number]: number } = {};
-
-    for (const num of arr) {
-        if (countMap[num] !== undefined) {
-            countMap[num]++;
-        } else {
-            countMap[num] = 1;
-        }
-    }
-
-    let maxCount = 0;
-    let majorityElement: number | null = null;
-
-    for (const num in countMap) {
-        if (countMap[num] > maxCount) {
-            maxCount = countMap[num];
-            majorityElement = parseInt(num);
-        }
-    }
-
-    return majorityElement as number;
-}
-
-const result = majorityElement([1, 2, 3, 4, 5, 4, 3, 4]);
-console.log(result); 
+// Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
+// MASALAN: findLongestWord("I come from Uzbekistan") return "Uzbekistan"
