ae17844d5fc61c39b8c6bbda74c3733b12a0c008
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-06-26 17:44:28 +0900

    fix: tarints M-task

--- train.ts
+++ train.ts
@@ -1,26 +1,23 @@
-const animal_list = [
-  "fox",
-  "ant",
-  "bird",
-  "Lion",
-  "wolf",
-  "deer",
-  "bear",
-  "frog",
-  "hen",
-  "mole",
-  "duck",
-  "goat",
-  "dog",
-  "cat",
-  "bat",
-  "cỒck",
-  "cow",
-];
+// M-TASK: 
 
-function findAnimals(txt) {
-  let sameid;
+// Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+// MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+
+
+type NumberSquare = {
+  number: number;
+  square: number;
+};
+
+function getSquareNumbers(arr: number[]): NumberSquare[] {
+  const result: NumberSquare[] = [];
+  for (let i = 0; i < arr.length; i++) {
+      result.push({
+          number: arr[i],
+          square: arr[i] * arr[i]
+      });
+  }
+  return result;
 }
-
-const javob = findAnimals("shjfdshg");
-console.log("Javob", javob);
+const result = getSquareNumbers([1, 2, 3]);
+console.log(result);
