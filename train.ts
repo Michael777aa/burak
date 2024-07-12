85f8d460368f0a26cc9ef4ad08d238f1e82face2
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-07-12 20:53:10 +0900

    Task: T

--- train.ts
+++ train.ts
@@ -1,15 +1,37 @@
-// S-TASK:
+// T-TASK:
 
-// Shunday function yozing, u numberlardan tashkil topgan array qabul qilsin va osha numberlar orasidagi tushib qolgan sonni topib uni return qilsin
-// MASALAN: missingNumber([3, 0, 1]) return 2
+// Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
+// MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
 
-function missingNumber(nums: number[]): number {
-  nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== i) {
-      return i;
+function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+  const mergedArray: number[] = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      mergedArray.push(arr1[i]);
+      i++;
+    } else {
+      mergedArray.push(arr2[j]);
+      j++;
     }
   }
-  return nums.length;
+
+  while (i < arr1.length) {
+    mergedArray.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    mergedArray.push(arr2[j]);
+    j++;
+  }
+
+  return mergedArray;
 }
-console.log(missingNumber([3, 0, 1]));
+const array1 = [0, 3, 4, 31];
+const array2 = [4, 6, 30];
+const sortedArray = mergeSortedArrays(array1, array2);
+
+console.log(sortedArray);
