8421793168c3b47b7c62de594e5d829f6a53ec8c
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-07-21 14:56:07 +0900

    feat: X-task

--- train.ts
+++ train.ts
@@ -1,19 +1,30 @@
-// W-TASK:
+// X-TASK:
 
-// Shunday function yozing, uni array va number parametrlari bolsin. Function arrayni numberda berilgan uzunlikda kesib bolaklarga ajratilgan array holatida qaytarsin
-// MASALAN: chunkArray([1,2,3,4,5,6,7,8,9,10], 3) return [[1,2,3], [4,5,6], [7,8,9], [10]]
+// Shunday function yozing, uni object va string parapetrlari bolsin. Function string parametri object ichida necha marotaba takrorlanganligini qaytarsin (nested object bolsa ham sanasin)
+// MASALAN: countOccurrences({model: 'Bugatti', steer: {model: 'HANKOOK', size: 30}}, 'model') return 2
+function countOccurrences(obj: any, key: string): number {
+  if (typeof obj !== "object" || obj === null) {
+    return 0;
+  }
 
-function chunkArray<T>(array: T[], chunkSize: number): T[][] {
-  if (chunkSize <= 0) throw new Error("chunkSize must be greater than 0");
-
-  return array.reduce((acc: T[][], _, index: number) => {
-    if (index % chunkSize === 0) {
-      acc.push(array.slice(index, index + chunkSize));
+  return Object.keys(obj).reduce((count, k) => {
+    let valueCount = 0;
+    if (k === key) {
+      valueCount = 1;
     }
-    return acc;
-  }, []);
+    return count + valueCount + countOccurrences(obj[k], key);
+  }, 0);
 }
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const chunkSize = 3;
-const chunkedArray = chunkArray(array, chunkSize);
-console.log(chunkedArray);
+
+const data = {
+  model: "Bugatti",
+  steer: {
+    model: "HANKOOK",
+    size: 30,
+    details: {
+      model: "SomeModel",
+    },
+  },
+};
+
+console.log(countOccurrences(data, "model"));
