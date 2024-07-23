d2fff6d43f030f4423f9c14f1765d69cdc1cdcf4
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-07-24 01:01:05 +0900

    Y-TASK

--- train.ts
+++ train.ts
@@ -1,30 +1,11 @@
-// X-TASK:
+// Y-TASK:
 
-// Shunday function yozing, uni object va string parapetrlari bolsin. Function string parametri object ichida necha marotaba takrorlanganligini qaytarsin (nested object bolsa ham sanasin)
-// MASALAN: countOccurrences({model: 'Bugatti', steer: {model: 'HANKOOK', size: 30}}, 'model') return 2
-function countOccurrences(obj: any, key: string): number {
-  if (typeof obj !== "object" || obj === null) {
-    return 0;
-  }
+// Shunday function yozing, uni 2 ta array parapetri bolsin. Function ikkala arrayda ham ishtirok etgan qiymatlarni bir arrayda qaytarsin
+// MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3]
 
-  return Object.keys(obj).reduce((count, k) => {
-    let valueCount = 0;
-    if (k === key) {
-      valueCount = 1;
-    }
-    return count + valueCount + countOccurrences(obj[k], key);
-  }, 0);
+function findIntersection<T>(array1: T[], array2: T[]): T[] {
+  return array1.filter((value) => array2.includes(value));
 }
 
-const data = {
-  model: "Bugatti",
-  steer: {
-    model: "HANKOOK",
-    size: 30,
-    details: {
-      model: "SomeModel",
-    },
-  },
-};
-
-console.log(countOccurrences(data, "model"));
+const result = findIntersection([1, 2, 3], [3, 2, 0]);
+console.log(result);
