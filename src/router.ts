ed97901d42f770e934e32d76f2d8d920e856f995
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-07-23 22:09:39 +0900

    feat: develop getproducts rest API

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
+import productController from "./controllers/product.controller";
 /** Member **/
 router.get("/member/restaurant", memberController.getRestaurant);
 router.post("/member/login", memberController.login);
@@ -26,5 +27,6 @@ router.post(
 router.get("/member/top-users", memberController.getTopUsers);
 /** Product **/
 
+router.get("/product/all", productController.getProducts);
 /** Order **/
 export default router;
