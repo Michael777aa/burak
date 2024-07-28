9db7e624d427bfb5832db5e22d7b3bdc3708b10b
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-07-28 19:07:45 +0900

    ZB-Task

--- train.ts
+++ train.ts
@@ -1,17 +1,11 @@
-// Z-TASK:
+// B-TASK:
 
-// Shunday function yozing, uni sonlardan tashkil topgan array qabul qilsin. Function arraydagi juft sonlarni yigindisini qaytarsin
-// MASALAN: sumEvens([1,2,3]) return 2
+// Shunday function yozing, uni 2 ta number parametri bolsin va berilgan sonlar orasidan random raqam return qilsin
+// MASALAN: randomBetween(30, 50) return 45
 
-function sumEvens(numbers: number[]): number {
-  let sum = 0;
-
-  for (const num of numbers) {
-    if (num % 2 === 0) {
-      sum += num;
-    }
-  }
-
-  return sum;
+function randomBetween(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(sumEvens([1, 2, 3]));
+
+const randomNumber = randomBetween(30, 50);
+console.log(randomNumber);
